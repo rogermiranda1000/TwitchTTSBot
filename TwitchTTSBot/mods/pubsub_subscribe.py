322b96d8294bf8d74ff4de97b9f2d5d9764e183f
rogermiranda1000 <rogermirandaperez@hotmail.com>
2023-10-01 11:56:04 +0000

    Fix: event playing twice

--- TwitchTTSBot/mods/pubsub_subscribe.py
+++ TwitchTTSBot/mods/pubsub_subscribe.py
@@ -5,6 +5,9 @@ from typing import Tuple
 from bot import TwitchTTSBot
 
 class PubSubSubscriberMod(Mod):
+    def __init__(self):
+        self._last_request = None
+
     async def on_connected(self):
         access_token = PubSubSubscriberMod.__GetAccessToken()
         await get_pubsub().listen_to_channel(access_token[0], [ PubSubTopics.channel_points,PubSubTopics.community_channel_points ], access_token=access_token[1])
@@ -32,5 +35,10 @@ class PubSubSubscriberMod(Mod):
     
     async def on_pubsub_custom_channel_point_reward(self, _: PubSubData, data: PubSubPointRedemption):
         print(f"[v] Point redeem call: {data.reward_title}, '{data.user_input}' (by {data.user_login_name})")
+
         if data.reward_title == self._redeem_name:
+            if self._last_request is not None and self._last_request.user_login_name == data.user_login_name and self._last_request.user_input == data.user_input:
+                return
+
+            self._last_request = data
             await TwitchTTSBot.instance()._on_channel_points_redeemed(data.user_login_name, data.user_input)
