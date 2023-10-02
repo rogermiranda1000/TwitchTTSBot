5b4067b8dc712a03dd5a1eae7d8cef2a891afe60
rogermiranda1000 <rogermirandaperez@hotmail.com>
2023-10-02 18:15:43 +0000

    Feature: voice name case insensitive

--- TwitchTTSBot/bot_factory.py
+++ TwitchTTSBot/bot_factory.py
@@ -117,7 +117,7 @@ def _generate_voice_splits(segment: TTSSegment, models: List[RVCTTSSynthesizer])
 
         regex_pattern = re.compile(r'(?: |^)' + re.escape(model.model.alias) + r': ') # find pattern (sanitized), followed by spaces or begin/end
         for segment in prev:
-            split = re.split(regex_pattern, segment.text)
+            split = re.split(regex_pattern, segment.text, re.IGNORECASE)
 
             more_split = split[0].strip()
             if len(more_split) > 0:
