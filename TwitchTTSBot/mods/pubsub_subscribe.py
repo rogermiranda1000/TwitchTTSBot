461fc003d251e3a53e23f4ab9ca52a4ff1476f9b
rogermiranda1000 <rogermirandaperez@hotmail.com>
2023-10-04 20:19:45 +0000

    Fix: double redeems

--- TwitchTTSBot/mods/pubsub_subscribe.py
+++ TwitchTTSBot/mods/pubsub_subscribe.py
@@ -1,16 +1,16 @@
 from twitchbot import Mod
-from twitchbot import PubSubTopics,get_pubsub,PubSubData,PubSubPointRedemption,PubSubModerationAction
+from twitchbot import PubSubTopics,get_pubsub,PubSubData,PubSubPointRedemption
 import json
 from typing import Tuple
 from bot import TwitchTTSBot
 
 class PubSubSubscriberMod(Mod):
     def __init__(self):
-        self._last_request = None
+        super().__init__()
 
     async def on_connected(self):
         access_token = PubSubSubscriberMod.__GetAccessToken()
-        await get_pubsub().listen_to_channel(access_token[0], [ PubSubTopics.channel_points,PubSubTopics.community_channel_points ], access_token=access_token[1])
+        await get_pubsub().listen_to_channel(access_token[0], [ PubSubTopics.channel_points ], access_token=access_token[1])
 
         self._redeem_name = PubSubSubscriberMod._GetRedeemName()
 
@@ -37,9 +37,5 @@ class PubSubSubscriberMod(Mod):
         print(f"[v] Point redeem call: {data.reward_title}, '{data.user_input}' (by {data.user_login_name})")
 
         if data.reward_title == self._redeem_name:
-            if self._last_request is not None and self._last_request.user_login_name == data.user_login_name and self._last_request.user_input == data.user_input:
-                return
-
-            self._last_request = data
             if len(data.user_input.strip()) > 0:
                 await TwitchTTSBot.instance()._on_channel_points_redeemed(data.user_login_name, data.user_input)
